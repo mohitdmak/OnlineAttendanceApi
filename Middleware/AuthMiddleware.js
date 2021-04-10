aaa4019dea18b2a0bbfe2c43b1aa55dc37d333ab
mohitdmak <mdmohitrc@gmail.com>
2021-04-10 13:29:24 +0530

    route changes

--- Middleware/AuthMiddleware.js
+++ Middleware/AuthMiddleware.js
@@ -9,6 +9,7 @@ const reqAuth = (req, res, next) => {
             if(err){
                 console.log('JWT token was not valid, redirecting . . .');
                 res.status(403).json({response: 'You are not authorized to access this page !'});
+                res.end();
             }
             else{
                 console.log('Allowing authenticated User . . .');
@@ -19,6 +20,7 @@ const reqAuth = (req, res, next) => {
     }
     else{
         res.status(403).json({response: 'You are not authorized to access this page !'});
+        res.end();
     }
 }
 
