a5d736e9cd8251c646bfd11a3528e55ead692ceb
Restyled.io <commits@restyled.io>
2021-04-12 17:56:19 +0000

    Restyled by clang-format

--- models/Classroom.js
+++ models/Classroom.js
@@ -2,24 +2,13 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
-
 // Creating Classroom schema
 const Classroomschema = new schema({
-    Subject: {
-        type: String,
-        required: true
-    },
-    Teacher: {
-        type: schema.Types.ObjectId,
-        ref: "teacher" 
-    },
-    Students: [{
-        type: schema.Types.ObjectId,
-        ref: "student"
-    }]
+  Subject : {type : String, required : true},
+  Teacher : {type : schema.Types.ObjectId, ref : "teacher"},
+  Students : [ {type : schema.Types.ObjectId, ref : "student"} ]
 });
 
-
-//Exporting Teacher model.
+// Exporting Teacher model.
 const Classroom = mongoose.model('classroom', Classroomschema);
 module.exports = Classroom;
